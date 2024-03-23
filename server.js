726cf7b09c4d4da909a422dadad511dfe3a7a51e
MariusGhiniei <marius_iulian_23@yahoo.com>
2024-03-24 01:34:25 +0200

    express + db connection

--- server.js
+++ server.js
@@ -4,13 +4,12 @@ const mongoose = require('mongoose')
 mongoose.set('strictQuery', true)
 
 app.listen(3000, function check(error){
-    if(error)
-        console.log("Server error ...");
-    else console.log("Server started!");
+    if(error) console.log("Server error ...")
+    else console.log("Server started!")
 })
 
 mongoose.connect("mongodb://localhost:27017/ShareMyRecipe", {useNewUrlParser: true, useUnifiedTopology: true},
 function checkDb(error){
     if(error) console.log("DB conection error ...")
-    else console.log("DB conected");
+    else console.log("DB conected")
 })
