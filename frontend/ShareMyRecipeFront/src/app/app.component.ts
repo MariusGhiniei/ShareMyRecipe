6af92fd50054ae4baaa2501afe5f2dff19813abd
MariusGhiniei <marius_iulian_23@yahoo.com>
2024-03-29 06:08:23 +0200

    some angular comps and router

--- frontend/ShareMyRecipeFront/src/app/app.component.ts
+++ frontend/ShareMyRecipeFront/src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
+import { register } from 'module';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
